b644f40bc1fb24acfd4d970b94ed47da7cabbb79
Toby Clemson <tobyclemson@gmail.com>
2020-09-16 15:21:26 +0100

    Remove name and allow terraform to autogenerate.

--- alb.tf
+++ alb.tf
@@ -1,7 +1,6 @@
 resource "aws_lb" "load_balancer" {
   load_balancer_type = "application"
 
-  name = "${var.component}-${var.deployment_identifier}"
   subnets = var.subnet_ids
   security_groups = local.security_groups.default.associate == "yes" ? [aws_security_group.default["default"].id] : null
 
